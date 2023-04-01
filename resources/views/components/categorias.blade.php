34acb3c989f6b772c2badbabcdaf70dd2a5da5b3
allancosta7 <allan.costa.7@hotmail.com>
2023-03-31 22:08:13 -0300

    Usando o layout corretamente

---
+++ resources/views/components/categorias.blade.php
@@ -0,0 +1,57 @@
+<div class="row titulo-card-style-categoria">
+    <div class="col-12">
+        <h2>
+            <center>Categorias</center>
+        </h2>
+    </div>
+</div>
+<div class="row titulo-card-style-categoria">
+    <div class="col-lg-4 col-12">
+        <div class="card card-style-categoria" style="width: 18rem;">
+            <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-12">
+        <div class="card card-style-categoria" style="width: 18rem; ">
+            <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-12">
+        <div class="card card-style-categoria" style="width: 18rem;">
+            <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-12">
+        <div class="card card-style-categoria" style="width: 18rem;">
+            <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-12">
+        <div class="card card-style-categoria" style="width: 18rem; ">
+            <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-12">
+        <div class="card card-style-categoria" style="width: 18rem;">
+            <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,41 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carda Shopping</title>
-
-    <!-- Fonte -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
-    <!-- Estilos -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/styleMenu.css">
-    <link rel="stylesheet" href="/css/styleCarrosel.css">
+<x-layout>
     <link rel="stylesheet" href="/css/styleCategoria.css">
-
-    <!-- Scripts (jQuery não pode ser o slim que vem do Boostrap) -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/bf7e05c402.js" crossorigin="anonymous"></script>
-    <!-- Progress Bar -->
-    <script src="js/progressbar.min.js"></script>
-    <!-- Parallax -->
-    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
-
-
-</head>
-
-<body>
-
-    <x-layout>
-
-
-    </x-layout>
-
     <div class="container-fluid">
 
         <!-- inicio carrousel -->
@@ -73,44 +37,8 @@
         </div>
 
         <!-- fim carrousel -->
-        <!-- inicio categoria -->
-        <div class="row card-style-categoria">
-            <div class="col-12">
-                <h2>
-                    <center>Categorias</center>
-                </h2>
-            </div>
-        </div>
-        <div class="row card-style-categoria">
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://flordejambu.com/wp-content/uploads/2022/05/acai.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- fim categoria -->
+        
+        @include('components.categorias')
     </div>
 
-
-</body>
-
-</html>
+</x-layout>
