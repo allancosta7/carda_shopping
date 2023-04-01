c36209fe6d0b26ff656cef0b19d6d1f77772e4a9
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-03-31 22:27:05 -0300

    Exclusão do tutorial

--- resources/views/tutorial-rapidao.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="tuto">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil hic sed consequatur ratione placeat pariatur
-  repudiandae, eligendi architecto at qui? Dicta tempora et ipsa impedit nisi reprehenderit soluta quo voluptatum.
-</div>
