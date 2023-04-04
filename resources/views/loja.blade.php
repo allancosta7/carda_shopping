f06e07dc1dc705721cb3a4ee3e42545307a32443
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-04-03 21:55:40 -0300

    uso do layout na página da loja

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -1,50 +1,30 @@
-<?php
-
-?>
-
-<!DOCTYPE html>
-<html lang="pt">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/bootstrap.css">
+<x-layout>
   <link rel="stylesheet" href="/css/loja.css">
-  <title>lojas</title>
-
-</head>
-
-<body>
-  <x-layout>
-    <div class="container-fluid">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="logo col-3">
+        <img src="/img/logo.jpg">
+      </div>
+      <div class="sobre col-9 text-align-center">
+        <p class="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+          has been
+          the
+          industry's
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+          makectronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+          containing
+          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus P
+          a type specimen book. It has survived not only five centuries, but also the leap into eleageMaker including
+          versions
+          of Lorem Ipsum.</p>
+      </div>
       <div class="row">
-        <div class="logo col-3">
-          <img src="/img/logo.jpg">
-        </div>
-        <div class="sobre col-9 text-align-center">
-          <p class="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been
-            the
-            industry's
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-            makectronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-            containing
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus P
-            a type specimen book. It has survived not only five centuries, but also the leap into eleageMaker including
-            versions
-            of Lorem Ipsum.</p>
-        </div>
-        <div class="row">
-          <div class="produtos">
-            <h2>Produtos</h2>
-          </div>
+        <div class="produtos">
+          <h2>Produtos</h2>
         </div>
       </div>
     </div>
+  </div>
 
-  </x-layout>
-</body>
-
-</html>
+</x-layout>
