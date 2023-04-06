700efa55828f7830c2f2651f6c17010aca5ae575
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-04-06 20:47:08 -0300

    Atualização do arquivo loja

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -2,29 +2,63 @@
   <link rel="stylesheet" href="/css/loja.css">
   <div class="container-fluid">
     <div class="row">
-      <div class="logo col-3">
-        <img src="/img/logo.jpg">
+      <div class="logo col-3 align-items-center">
+        <img src="/img/mac.png" class="mac">
       </div>
-      <div class="sobre col-9 text-align-center">
-        <p class="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-          has been
-          the
-          industry's
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-          makectronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-          containing
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus P
-          a type specimen book. It has survived not only five centuries, but also the leap into eleageMaker including
-          versions
-          of Lorem Ipsum.</p>
-      </div>
-      <div class="row">
-        <div class="produtos">
-          <h2>Produtos</h2>
-        </div>
+      <div class="sobre col-9 align-text-center">
+        <p class="descricao">LMcDonald's Corporation é a maior cadeia mundial de restaurantes de fast food de
+          hambúrguer, servindo cerca de 68 milhões de clientes por dia em 119 países através de 37 mil pontos de venda.
+          Com sede nos Estados Unidos, a empresa começou em 1940 como uma churrascaria operada por Richard e Maurice
+          McDonald.</p>
       </div>
     </div>
+    <div class="row offset-1">
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="/img/pao-mortadela.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pão com mortadela</h5>
+            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+            <a href="#" class="btn btn-primary">R$4,99</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="/img/pao-mortadela.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pão com mortadela</h5>
+            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+            <a href="#" class="btn btn-primary">R$4,99</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="/img/pao-mortadela.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pão com mortadela</h5>
+            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+            <a href="#" class="btn btn-primary">R$4,99</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="/img/pao-mortadela.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pão com mortadela</h5>
+            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+            <a href="#" class="btn btn-primary">R$4,99</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
   </div>
 
 </x-layout>
