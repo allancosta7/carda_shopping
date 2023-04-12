9945ea8a1d6b923c3673a03df6b6a524e603470f
allancosta7 <allan.costa.7@hotmail.com>
2023-04-12 20:04:44 -0300

    ajustado categoria e criado arquivo proprio para o carousel

--- resources/views/components/categorias.blade.php
+++ resources/views/components/categorias.blade.php
@@ -1,47 +1,65 @@
 <div class="row titulo-card-style-categoria">
     <div class="col-12">
-        <h2>
-            <center>Categorias</center>
-        </h2>
+        <h2>Categorias</h2>
     </div>
 </div>
 <div class="row titulo-card-style-categoria">
 
-    <div class="col-lg-3 col-12">
+    <div class="col-lg-2 col-4">
         <div class="card card-style-categoria">
             <a href="http://"><img src="\img\mac.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">McDonald's</h5>
+                <div>
+                    <p>McDonald's</p>
                 </div>
             </a>
         </div>
     </div>
 
-    <div class="col-lg-3 col-12">
+    <div class="col-lg-2 col-4">
         <div class="card card-style-categoria">
             <a href="http://"><img src="\img\mac.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">McDonald's</h5>
+                <div>
+                    <p>McDonald's</p>
                 </div>
             </a>
         </div>
     </div>
 
-    <div class="col-lg-3 col-12">
+    <div class="col-lg-2 col-4">
         <div class="card card-style-categoria">
             <a href="http://"><img src="\img\mac.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">McDonald's</h5>
+                <div>
+                    <p>McDonald's</p>
                 </div>
             </a>
         </div>
     </div>
 
-    <div class="col-lg-3 col-12">
+    <div class="col-lg-2 col-4">
         <div class="card card-style-categoria">
             <a href="http://"><img src="\img\mac.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">McDonald's</h5>
+                <div>
+                    <p>McDonald's</p>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-2 col-4">
+        <div class="card card-style-categoria">
+            <a href="http://"><img src="\img\mac.png" class="card-img-top" alt="...">
+                <div>
+                    <p>McDonald's</p>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-2 col-4">
+        <div class="card card-style-categoria">
+            <a href="http://"><img src="\img\mac.png" class="card-img-top" alt="...">
+                <div>
+                    <p>McDonald's</p>
                 </div>
             </a>
         </div>
