10c6c8b682e0406b157cb7bc969c884238b652ec
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-04-13 20:48:30 -0300

    deixando o arquivo loja responsivo

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -1,102 +1,64 @@
 <x-layout>
   <link rel="stylesheet" href="/css/loja.css">
   <div class="inicio container">
-    <div class="loja row">
-      <div class="logo col-2">
-        <img src="/img/mac.png" class="imagem">
-      </div>
-      <div class="sobre col-10">
-        <h2 class="nomeLoja">McDonald's</h2>
-        <p class="descricao">McDonald's Corporation é a maior cadeia mundial de restaurantes de fast food de
-          hambúrguer, servindo cerca de 68 milhões de clientes por dia em 119 países através de 37 mil pontos de venda.
-          Com sede nos Estados Unidos, a empresa começou em 1940 como uma churrascaria operada por Richard e Maurice
-          McDonald.</p>
-      </div>
-    </div>
-    <br>
-    <div class="produtos row">
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
+
+    <div class="card mb-3">
+      <div class="row">
+        <div class="loja col-md-4">
+          <img src="/img/mac.png" class="imagem img-fluid" alt="...">
+        </div>
+        <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
+            <h2 class="nomeLoja card-title">McDonald's</h2>
+            <p class="descricao card-text">McDonald's Corporation é a maior cadeia mundial de restaurantes de fast food
+              de
+              hambúrguer, servindo cerca de 68 milhões de clientes por dia em 119 países através de 37 mil pontos de
+              venda.
+              Com sede nos Estados Unidos, a empresa começou em 1940 como uma churrascaria operada por Richard e Maurice
+              McDonald.</p>
           </div>
         </div>
       </div>
-
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
-          </div>
-        </div>
-      </div>
-
     </div>
 
     <br>
-    <div class="row justify-content-center">
 
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
-          </div>
+    <div class="card-group">
+      <div class="card">
+        <img src="/img/pao-mortadela.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Pão com mortadela</h5>
+          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+          <a href="#" class="btn btn-primary">R$4,99</a>
         </div>
       </div>
-
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
-          </div>
+      <div class="card">
+        <img src="/img/pao-mortadela.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Pão com mortadela</h5>
+          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+          <a href="#" class="btn btn-primary">R$4,99</a>
         </div>
       </div>
-
-      <div class="col-3">
-        <div class="card">
-          <img src=" /img/pao-mortadela.jpg" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">Pão com mortadela</h5>
-            <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-            <a href="#" class="btn btn-primary">R$4,99</a>
-          </div>
+      <div class="card">
+        <img src="/img/pao-mortadela.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Pão com mortadela</h5>
+          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+          <a href="#" class="btn btn-primary">R$4,99</a>
+        </div>
+      </div>
+      <div class="card">
+        <img src="/img/pao-mortadela.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Pão com mortadela</h5>
+          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
+          <a href="#" class="btn btn-primary">R$4,99</a>
         </div>
       </div>
     </div>
+
+
 
 
   </div>
