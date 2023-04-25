96d6ada741c96f6cf9acb367d59770d5902abfe1
allancosta7 <allan.costa.7@hotmail.com>
2023-04-24 22:23:16 -0300

    criação dos models

---
+++ app/Models/loja.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class loja extends Model
+{
+    use HasFactory;
+    protected $table = 'loja';
+    public $timestamps = false;
+}
---
+++ app/Models/produto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class produto extends Model
+{
+    use HasFactory;
+    protected $table = 'produto';
+    public $timestamps = false;
+}
