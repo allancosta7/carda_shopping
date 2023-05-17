d5aa17de1f91e7934da2e7969c7cc54b529d9d0a
allancosta7 <allan.costa.7@hotmail.com>
2023-05-16 21:51:18 -0300

    Configurado back end da pagina loja

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -1,3 +1,13 @@
+<?php
+
+use App\Models\Loja;
+use App\Models\Produto;
+
+$loja = Loja::find(1);
+$produtos = Produto::where('id_loja', '=', 1)->get();
+
+?>
+
 <x-layout>
   <link rel="stylesheet" href="/css/loja.css">
   <div class="inicio sombra_borda container border">
@@ -5,17 +15,12 @@
     <div class="card mb-3">
       <div class="row">
         <div class="loja col-md-4">
-          <img src="/img/mac.png" class="imagem img-fluid" alt="...">
+          <img src="/img/{{$loja->logo}}" class="imagem img-fluid" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h2 class="nomeLoja card-title">McDonald's</h2>
-            <p class="descricao card-text">McDonald's Corporation é a maior cadeia mundial de restaurantes de fast food
-              de
-              hambúrguer, servindo cerca de 68 milhões de clientes por dia em 119 países através de 37 mil pontos de
-              venda.
-              Com sede nos Estados Unidos, a empresa começou em 1940 como uma churrascaria operada por Richard e Maurice
-              McDonald.</p>
+            <h2 class="nomeLoja card-title">{{$loja->nome}}</h2>
+            <p class="descricao card-text">{{$loja->descricao}}</p>
           </div>
         </div>
       </div>
@@ -24,38 +29,18 @@
     <br>
 
     <div class="card-group">
+
+      @foreach ($produtos as $produto) 
       <div class="card">
-        <img src="/img/pao-mortadela.jpg" class="card-img-top">
+        <img src="/img/{{$produto->imagem}}" class="card-img-top">
         <div class="card-body">
-          <h5 class="card-title">Pão com mortadela</h5>
-          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-          <a href="#" class="btn btn-primary">R$4,99</a>
+          <h5 class="card-title">{{$produto->nome}}</h5>
+          <p class="card-text">Descrição: {{$produto->descricao}}</p>
+          <a href="#" class="btn btn-primary">R${{$produto->preco}}</a>
         </div>
       </div>
-      <div class="card">
-        <img src="/img/pao-mortadela.jpg" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">Pão com mortadela</h5>
-          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-          <a href="#" class="btn btn-primary">R$4,99</a>
-        </div>
-      </div>
-      <div class="card">
-        <img src="/img/pao-mortadela.jpg" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">Pão com mortadela</h5>
-          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-          <a href="#" class="btn btn-primary">R$4,99</a>
-        </div>
-      </div>
-      <div class="card">
-        <img src="/img/pao-mortadela.jpg" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">Pão com mortadela</h5>
-          <p class="card-text">Descrição: Pão francês com manteiga e mortadela frio</p>
-          <a href="#" class="btn btn-primary">R$4,99</a>
-        </div>
-      </div>
+      @endforeach
+
     </div>
 
 
