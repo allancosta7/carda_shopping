1bbe5055428526adec446157ed95ed10af418fc0
allancosta7 <allan.costa.7@hotmail.com>
2023-05-29 19:43:24 -0300

    css devs

--- resources/views/devs.blade.php
+++ resources/views/devs.blade.php
@@ -1,28 +1,6 @@
 <x-layout>
 
-    <style>
-        /* MARKETING CONTENT
--------------------------------------------------- */
-
-/* Center align the text within the three columns below the carousel */
-.marketing{
-    margin-top: 15px;
-}
-.marketing .col-lg-4 {
-  margin-bottom: 1.5rem;
-  text-align: center;
-}
-.marketing h2 {
-  font-weight: 400;
-}
-/* rtl:begin:ignore */
-.marketing .col-lg-4 p {
-  margin-right: .75rem;
-  margin-left: .75rem;
-}
-/* rtl:end:ignore */
-
-    </style>
+  <link href="/css/devs.css" rel="stylesheet">
 
 <section class="py-5 text-center container">
     <div class="row py-lg-5">
@@ -50,7 +28,7 @@
 
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-        <h2>Thales</h2>
+        <h2>Tales</h2>
         <p>And lastly this, the third column of representative placeholder content.</p>
       </div><!-- /.col-lg-4 -->
 
